b294d2c7d160318fa9b86b570f9b1122f1d63922
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-03-07 21:11:39 +0100

    update basic

---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+    return (
+        <header>
+            <img src="../../public/globe.png" alt="Globe" />
+            <h1>my travel journal.</h1>
+        </header>
+    )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
+import { App } from './App.jsx'
 
 const root = createRoot(document.getElementById('root'))
 
-console.log(root)
+root.render(<App />)
